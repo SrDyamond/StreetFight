08161c20950688c6729f8a3155714d415b316bb1
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-12-16 12:27:56 +0100

    Bot de following automatico creado

--- BotTwitter/BotPeleonCallejero/PublicMesage.py
+++ BotTwitter/BotPeleonCallejero/PublicMesage.py
@@ -5,7 +5,6 @@ auth = tweepy.OAuthHandler("lfcWo3XjbtVo0WRTCrGkhaaVE",
     "GaFuFQ0uKbhPYqWqNDmrPttM6LdfTZVNlSTUNnEXNHWweOUy25")
 auth.set_access_token("1338837146363375616-q43ZTHb9lqPcoK7oDtQ22ewfGsiuNE",
     "mHnwQigRHqJabpeikdmFDUfu2cGkMzeXJOpUznPqddyX0")
-
 # Create API object
 api = tweepy.API(auth)
 
