169666479ce35d25aecd64d0c16b67cd354404ca
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-12-16 11:08:29 +0100

    Credenciales para la app de twitter creadas,prueba de Autentificacion ok

---
+++ BotTwitter/Botillo.py
@@ -0,0 +1,15 @@
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("lfcWo3XjbtVo0WRTCrGkhaaVE",
+    "GaFuFQ0uKbhPYqWqNDmrPttM6LdfTZVNlSTUNnEXNHWweOUy25")
+auth.set_access_token("1338837146363375616-q43ZTHb9lqPcoK7oDtQ22ewfGsiuNE",
+    "mHnwQigRHqJabpeikdmFDUfu2cGkMzeXJOpUznPqddyX0")
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
