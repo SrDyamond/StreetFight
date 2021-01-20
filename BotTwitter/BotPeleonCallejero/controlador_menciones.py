1dc1e5a978d2fcadb054c751ce63e15a49e339f7
Dyango <dsanchezr@fpcoruna.afundacion.org>
2021-01-20 13:35:37 +0100

    #20:Metodo creado en controlador_menciones

--- BotTwitter/BotPeleonCallejero/controlador_menciones.py
+++ BotTwitter/BotPeleonCallejero/controlador_menciones.py
@@ -5,6 +5,14 @@ import json
 import time
 import random
 
-def parse_text(texto):
-    nombre_bot="@StreetFightSP"
-    print (texto)
+def parse_text(texto,user,api):
+	texto_separado=texto.split()
+
+	try:
+		if (texto_separado[1] == "Ayudame!"):
+			api.update_status("Ayuda\n- Top clanes <número>\n- Top usuarios <número>\n- Info del clan <nombre>\n- Info del usuario <nombre>\n @"+str(user))
+					#metodo
+			#AÑADIR TODAS LAS COMPROBACIONES PARA COMANDOS
+	except tweepy.TweepError as error:
+		if error.api_code == 187:
+			print('duplicate message')
