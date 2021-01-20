32ce593300eccad68b6b77899150e3462918f616
Dyango <dsanchezr@fpcoruna.afundacion.org>
2021-01-20 14:44:04 +0100

    #20:Creado metodo para saber si la posicion 3 esta vacia,try except para saber si es un int

--- BotTwitter/BotPeleonCallejero/controlador_menciones.py
+++ BotTwitter/BotPeleonCallejero/controlador_menciones.py
@@ -5,14 +5,31 @@ import json
 import time
 import random
 
+def existe_posicion(array, posicion):
+	try:
+		a = array[posicion]
+		return True
+	except IndexError:
+		return False
+
+
 def parse_text(texto,user,api):
 	texto_separado=texto.split()
+	is_int=True;
+
+	if existe_posicion(texto_separado,3):
+		try :
+			cantidad = int(texto_separado[3])
+		except ValueError  :
+			print("Este string no contiene un int")
 
 	try:
 		if (texto_separado[1] == "Ayuda!"):
 			api.update_status("Ayuda\n- Top clanes <número>\n- Top usuarios <número>\n- Info del clan <nombre>\n- Info del usuario <nombre>\n @"+str(user))
 			print("Mensaje enviado a"+str())
 			#AÑADIR TODAS LAS COMPROBACIONES PARA COMANDOS
+		if (texto_separado[1] == "Top" and texto_separado[2] == "clanes"):
+			print(cantidad)
 	except tweepy.TweepError as error:
 		if error.api_code == 187:
 			print('duplicate message')
