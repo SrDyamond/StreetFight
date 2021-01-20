f7966bf09771906c96cf947aff209fa4711d2bbe
dyango.sanchez <dsanchezr@fpcoruna.afundacion.org>
2021-01-20 23:57:02 +0100

    #20:Añadida variable is_int para combrobaciones.Añadido else para mandar tweet al usuario si el comando es erroneo,tweet aun no creado.

--- BotTwitter/BotPeleonCallejero/controlador_menciones.py
+++ BotTwitter/BotPeleonCallejero/controlador_menciones.py
@@ -15,11 +15,12 @@ def existe_posicion(array, posicion):
 
 def parse_text(texto,user,api):
 	texto_separado=texto.split()
-	is_int=True;
+	is_int=False
 
 	if existe_posicion(texto_separado,3):
 		try :
 			cantidad = int(texto_separado[3])
+			is_int=True
 		except ValueError  :
 			print("Este string no contiene un int")
 
@@ -28,8 +29,11 @@ def parse_text(texto,user,api):
 			api.update_status("Ayuda\n- Top clanes <número>\n- Top usuarios <número>\n- Info del clan <nombre>\n- Info del usuario <nombre>\n @"+str(user))
 			print("Mensaje enviado a"+str())
 			#AÑADIR TODAS LAS COMPROBACIONES PARA COMANDOS
-		if (texto_separado[1] == "Top" and texto_separado[2] == "clanes"):
+		if (texto_separado[1] == "Top" and texto_separado[2] == "clanes" and is_int==True):
 			print(cantidad)
+		else:
+			#Aqui va tweet diciendo que ponga bien el comando
+			print("Introduce comando valido")
 	except tweepy.TweepError as error:
 		if error.api_code == 187:
 			print('duplicate message')
