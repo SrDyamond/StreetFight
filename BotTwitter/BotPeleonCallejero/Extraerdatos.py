a0492ea86097a331f8628e6d365c5b719bfb6da3
Dyango <dsanchezr@fpcoruna.afundacion.org>
2021-01-20 14:01:21 +0100

    #20:Reorganizacion extraerdatos

--- BotTwitter/BotPeleonCallejero/Extraerdatos.py
+++ BotTwitter/BotPeleonCallejero/Extraerdatos.py
@@ -17,8 +17,9 @@ def get_tweets(api):
 	logger.info("Revisando menciones")
 	for mention in mentions:
 		parse_text(mention.text,mention.user.screen_name,api)
+
+
 	#FOLLOWFORFOLLOW AND SEND RANDOM WELCOME MESSAGE
-	#Comprobamos nuestros followers
 	logger.info("Revisando followers")
 	for follower in tweepy.Cursor(api.followers).items():
 	#si nos siguen y no los seguimos,los seguimos y lo mencinamos
