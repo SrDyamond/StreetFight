bac57a158b3cb533f0fc681035bfcaa122145ae2
Alfredo <arodriguezg@fpcoruna.afundacion.org>
2021-01-21 14:36:30 +0100

    Closes #21: Finalizada la definición de los endpoints de login y logout && revisado el funcionamiento de ambos

--- AppDjango/streetfight/rest_facade.py
+++ AppDjango/streetfight/rest_facade.py
@@ -12,11 +12,17 @@ from django.views.decorators.csrf import csrf_exempt
 from . import custom_error_response
 
 @csrf_exempt
-def login(request, username):
+def session(request, username):
     # Si recibimos una peticion que no es PUT, devolvemos un 405
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
+    if request.method == 'POST':
+        return login(request, username)
+    elif request.method == 'DELETE':
+        return logout(request, username)
+    else:
+        return HttpResponseNotAllowed(['POST', 'DELETE'])
 
+
+def login(request, username):
     try:
         request_body = json.loads(request.body)
         if not 'password_sha' in request_body:
@@ -47,3 +53,30 @@ def login(request, username):
         return JsonResponse(response, status=200)
 
     return JsonResponse(custom_error_response.BAD_PASSWORD, status=401)
+
+
+def logout(request, username):
+
+    if not 'sessioncookie' in request.headers:
+        return JsonResponse(custom_error_response.BAD_REQUEST, status=400)
+    
+    session_cookie = request.headers.get('sessioncookie')
+
+    try: # Recupero el user de base de datos, y si no existe devuelvo 404
+        user = Usuario.objects.get(nombre__exact=username)
+    except Usuario.DoesNotExist:
+        return JsonResponse(custom_error_response.NOT_FOUND, status=404)
+
+    session_list = Sesion.objects.filter(id_usuario__exact=user)
+
+    valid_session = False
+    for session in session_list:
+        if session.valor_cookie == session_cookie:
+            valid_session = True
+            session.delete()
+            break
+    
+    if valid_session:
+        return JsonResponse(custom_error_response.LOGOUT_OK, status=200)
+    else:
+        return JsonResponse(custom_error_response.BAD_COOKIE, status=401)
