2fc67c7dff043a71f9aecb13170270863d90b36a
David Mato <dmatog@fpcoruna.afundacion.org>
2021-01-25 10:39:28 +0100

    #23: URL creada.

--- AppDjango/streetfight/urls.py
+++ AppDjango/streetfight/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('user/<username>/session', rest_facade.session, name='session'),
     path('flag', rest_facade.flag, name='flag'),
     path('flag/<id_flag>', rest_facade.flag_by_id, name='flag_by_id'),
+    path('user',rest_facade.user, name ='user')
 ]
