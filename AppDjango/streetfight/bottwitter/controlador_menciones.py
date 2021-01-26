635b87e2ad97df52218ed856aead7a3491cc8650
Dyango <dsanchezr@fpcoruna.afundacion.org>
2021-01-26 12:01:06 +0100

    #20-Se imprime bien ya el nombre de clan,pero falla el if,siempre es true

--- AppDjango/streetfight/bottwitter/controlador_menciones.py
+++ AppDjango/streetfight/bottwitter/controlador_menciones.py
@@ -36,20 +36,21 @@ def parse_text(texto,user,api):
 			print("Mensaje de ayuda enviado a"+str(user))
 		#ComandoTopClanes
 		if (texto_separado[1] == "Top" and texto_separado[2] == "clanes" and is_int==True):
-			lista = Usuario.objects.all().order_by('-banderas_capturadas')[:cantidad]
+			lista = Usuario.objects.all()
 			lista_clan=[]
 			for usuario in lista:
-				print("ID:-"+str(usuario.id_clan))
-				print("Lista:-"+str(lista_clan))
-				if(usuario.id_clan != lista_clan):
-					lista_clan.append(usuario.id_clan)
-			# print(lista_clan)
+				print("ID:"+str(usuario.id_clan.nombre))
+				print("LISTA"+str(lista_clan))
+				if (usuario.id_clan.nombre != lista_clan):
+					lista_clan.append(usuario.id_clan.nombre)
 
+			print("LISTAFINAL"+str(lista_clan))
 		#ComandoTopUsuarios
 		elif (texto_separado[1] == "Top" and texto_separado[2] == "usuarios" and is_int==True):
 			lista = Usuario.objects.all().order_by('-banderas_capturadas')[:cantidad]
 			lista_usuarios=[]
 			for usuario in lista:
+				# print("Lista:-"+str(lista_usuarios))
 				lista_usuarios.append(usuario.nombre)
 			api.update_status("Hola @"+str(user)+" el top "+str(cantidad)+" usuarios  es: \n"+str(lista_usuarios))
 		else:
