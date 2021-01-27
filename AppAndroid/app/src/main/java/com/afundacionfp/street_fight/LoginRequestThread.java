6dcb24fcc2b629e4789bfdc26d05a54022dbb0d3
Alfredo <arodriguezg@fpcoruna.afundacion.org>
2021-01-27 19:06:16 +0100

    #26: empezando la estructura para hacer una comunicación con el api rest

---
+++ AppAndroid/app/src/main/java/com/afundacionfp/street_fight/LoginRequestThread.java
@@ -0,0 +1,72 @@
+package com.afundacionfp.street_fight;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class LoginRequestThread extends Thread {
+
+    private final String username;
+    private String password_sha = "";
+
+    public LoginRequestThread(String username, String password) {
+        this.username = username;
+        try {
+            this.password_sha = hashPassword(password);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+        crypt.reset();
+        crypt.update(password.getBytes(StandardCharsets.UTF_8));
+
+        return new BigInteger(1, crypt.digest()).toString(16);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            URL url = new URL("http://192.168.111.111:8000/user/" + username + "/session");
+            Log.d("####URL", url.toString());
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+            String jsonInputString = "{\"password_sha\": \"" + password_sha + "\"}";
+            Log.d("####REQUEST BODY", jsonInputString);
+
+            try(OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+//                System.out.println(response.toString());
+                Log.d("########RESPONSEEEEE", response.toString());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- AppAndroid/app/src/main/java/com/afundacionfp/street_fight/MainActivity.java
+++ AppAndroid/app/src/main/java/com/afundacionfp/street_fight/MainActivity.java
@@ -1,7 +1,12 @@
 package com.afundacionfp.street_fight;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +21,23 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         setContentView(R.layout.login_layout);
+
+        EditText input_username = findViewById(R.id.input_username);
+        EditText input_password = findViewById(R.id.input_password);
+
+        ImageButton button_login = findViewById(R.id.button_login);
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = input_username.getText().toString();
+                String password = input_password.getText().toString();
+                if (!username.equals("") && !password.equals("")) {
+                    Log.d("############USER, PASSWORD", username + ", " + password);
+                } else {
+                    Toast.makeText(MainActivity.this, "Introduce usuario y contraseña", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
