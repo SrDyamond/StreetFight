2b1dc1496cfa5e653ef2ab872c12ba012fde03b4
Alfredo <arodriguezg@fpcoruna.afundacion.org>
2021-01-27 17:15:44 +0100

    #26: cambiados los IDs correspondientes en la pantalla de login

--- AppAndroid/app/src/main/java/com/afundacionfp/street_fight/MainActivity.java
+++ AppAndroid/app/src/main/java/com/afundacionfp/street_fight/MainActivity.java
@@ -1,30 +1,10 @@
 package com.afundacionfp.street_fight;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.Window;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        setContentView(R.layout.pantalla_login_2);
+        setContentView(R.layout.login_layout);
     }
 
 }
