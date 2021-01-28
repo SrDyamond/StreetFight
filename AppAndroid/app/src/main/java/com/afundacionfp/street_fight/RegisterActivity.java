7f3a1ad8661ee187cd0dcef16fb00f9e10df7219
Alfredo <arodriguezg@fpcoruna.afundacion.org>
2021-01-28 21:13:21 +0100

    #28: creada la clase RegisterActivity para mostrar el register_layot

---
+++ AppAndroid/app/src/main/java/com/afundacionfp/street_fight/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.afundacionfp.street_fight;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        setContentView(R.layout.register_layout);
+
+    }
+
+}
