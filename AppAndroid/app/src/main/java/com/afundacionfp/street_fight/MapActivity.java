18359b1d02444184fae7dd76e73b09bf83bffe18
Alfredo <arodriguezg@fpcoruna.afundacion.org>
2021-02-04 22:21:35 +0100

    #46: funciona en mi movil

--- AppAndroid/app/src/main/java/com/afundacionfp/street_fight/MapActivity.java
+++ AppAndroid/app/src/main/java/com/afundacionfp/street_fight/MapActivity.java
@@ -9,6 +9,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
@@ -51,7 +52,7 @@ public class MapActivity extends AppCompatActivity implements ActivityCompat.OnR
 
         //load/initialize the osmdroid configuration, this can be done
         // Context ctx = getApplicationContext();
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(this));
         //setting this before the layout is inflated is a good idea
         //it 'should' ensure that the map has a writable location for the map cache, even without permissions
         //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
@@ -59,7 +60,8 @@ public class MapActivity extends AppCompatActivity implements ActivityCompat.OnR
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
         //tile servers will get you banned based on this string
 
-        //inflate and create the map
+        // Set content view
+
         setContentView(R.layout.map_layout);
 
         TextView textPlayerInfo = findViewById(R.id.activity_main_text);
@@ -67,8 +69,7 @@ public class MapActivity extends AppCompatActivity implements ActivityCompat.OnR
 
         //  Localization
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-
+        // es necesario comprobar si los permisos están concedidos
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -85,16 +86,16 @@ public class MapActivity extends AppCompatActivity implements ActivityCompat.OnR
         mapView = findViewById(R.id.map);
         mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setMultiTouchControls(true);
-        mapView.setMinZoomLevel(15.0); // Min zoom level
+        mapView.setMinZoomLevel(5.0); // si el 0 es lo mas alejado, hasta donde puedes ir para atrás
 
         iMapController = mapView.getController();
-        iMapController.setZoom(20.0); // Initial zoom
+        iMapController.setZoom(19.5); // si el 0 es lo mas alejado, a que nivel empiezas
 
-        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), mapView);
+        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()), mapView);
         mLocationOverlay.enableMyLocation();
         mapView.getOverlays().add(mLocationOverlay);
 
-        flags = new Flags(this,getResources(), mapView);
+        flags = new Flags(this, getResources(), mapView);
 
         requestPermissionsIfNecessary(new String[]{
                 // if you need to show the current location, uncomment the line below
@@ -103,6 +104,13 @@ public class MapActivity extends AppCompatActivity implements ActivityCompat.OnR
                 // WRITE_EXTERNAL_STORAGE is required in order to show the map
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         });
+
+        // Situamos el mapa en la última localización
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
+            iMapController.animateTo(geoPoint);
+        }
     }
 
     public void onUserButtonClick(View v) {
@@ -114,18 +122,18 @@ public class MapActivity extends AppCompatActivity implements ActivityCompat.OnR
     @Override
     public void onLocationChanged(Location location) {
         if (!paused) {
-            int fineLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-            int coarseLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
-            int writeExternalStorage = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-//            if (fineLocationPermission != PackageManager.PERMISSION_GRANTED && coarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
-            if (fineLocationPermission != PackageManager.PERMISSION_GRANTED || coarseLocationPermission != PackageManager.PERMISSION_GRANTED || writeExternalStorage != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-            }
+            Log.d("UPDATE", "Se actualiza la localización:" + location);
+//            int fineLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+//            int coarseLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+//            int writeExternalStorage = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//
+//            if (fineLocationPermission != PackageManager.PERMISSION_GRANTED || coarseLocationPermission != PackageManager.PERMISSION_GRANTED || writeExternalStorage != PackageManager.PERMISSION_GRANTED) {
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+//            }
 
-//            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            flags.sendFlagRequest(location.getLatitude(), location.getLongitude());
             GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
             iMapController.animateTo(geoPoint);
+            flags.sendFlagRequest(location.getLatitude(), location.getLongitude());
         }
     }
 
