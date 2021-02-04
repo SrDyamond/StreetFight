2566f1eda650f44c66ac90e1a38ef032fdf49b2d
Alfredo <arodriguezg@fpcoruna.afundacion.org>
2021-02-04 14:45:01 +0100

    [200~android:layout_width=220dp

--- AppAndroid/app/src/main/java/com/afundacionfp/street_fight/Client.java
+++ AppAndroid/app/src/main/java/com/afundacionfp/street_fight/Client.java
@@ -24,7 +24,7 @@ import java.util.Locale;
 public class Client {
 
     private static Client client = null;
-    public static final String DJANGOSERVERIP ="192.168.0.17:8000";
+    public static final String DJANGOSERVERIP ="192.168.0.101:8000";
     private final RequestQueue requestQueue;
 
     private Client(Context context){
