197eee8c8b9f71fc15f7f48b04673295a66c675f
Alfredo <arodriguezg@fpcoruna.afundacion.org>
2021-02-08 13:59:01 +0100

    #36: se muestra el mapa en el index

--- AppDjango/streetfight/views.py
+++ AppDjango/streetfight/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+# from django.shortcuts import render
+from django.http import HttpResponse
+# from django.views import generic
+from django.template import loader
 
 
 def index(request):
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # context = {'latest_question_list': latest_question_list}
     # return render(request, 'polls/index.html', context)
-    return render(request, 'streetfight/index.html')
+
+    # context = {}
+    # return render(request, 'streetfight/index.html', context)
+
+    template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
