60aedbd0cfdaa4c7f75f72576082aae05f45abb7
David <dmatog@fpcoruna.afundacion.org>
2021-02-08 13:32:29 +0100

    Mapa creado.

--- AppDjango/streetfight/views.py
+++ AppDjango/streetfight/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("index de Street Fight")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    # return render(request, 'polls/index.html', context)
+    return render(request, 'streetfight/index.html')
